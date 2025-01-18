19dc5988acebd807bfc29229ca62483d9a3e29c8
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-01-18 14:43:15 +0700

    tweak player list

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,34 @@
-
 function App() {
   return (
     <>
-    <header>
-      <img src="" alt="" />
-      <p>Coming soon...</p>
-    </header>
+      <header>
+        <img src="" alt="" />
+        <p>Coming soon...</p>
+      </header>
 
-    <main>
-      <div id="game-container">
-        <ol id="players">
-          <li>
-            <span className="player-name">Player 1</span>
-            <span className="player-symbol">X</span>
-          </li>
-          <li>
-            <span className="player-name">Player 2</span>
-            <span className="player-symbol">Y</span>
-          </li>
-        </ol>
-        Game Board
-      </div>
-    </main>
+      <main>
+        <div id="game-container">
+          <ol id="players">
+            <li>
+              <span className="player">
+                <span className="player-name">Player 1</span>
+                <span className="player-symbol">X</span>
+              </span>
+              <button>Edit</button>
+            </li>
+            <li>
+              <span className="player">
+                <span className="player-name">Player 2</span>
+                <span className="player-symbol">Y</span>
+              </span>
+              <button>Edit</button>
+            </li>
+          </ol>
+          Game Board
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
