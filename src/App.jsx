f7eaf95b79d77ef41306e727b90b3f8e21751993
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-01-18 14:40:46 +0700

    re-init App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
+    <>
+    <header>
+      <img src="" alt="" />
+      <p>Coming soon...</p>
+    </header>
+
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player 2</span>
+            <span className="player-symbol">Y</span>
+          </li>
+        </ol>
+        Game Board
+      </div>
+    </main>
+    </>
   )
 }
 
