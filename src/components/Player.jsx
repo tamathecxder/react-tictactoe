ee49a76d2e9f3d2d982b7f4199aa4d057a33539c
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-01-18 15:47:42 +0700

    add click handler, set element by a conditions

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,32 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+
+  let playerNameElement = <span className="player-name">{name}</span>;
+
+  if (isEditing) {
+    playerNameElement = (
+      <input
+        type="text"
+        placeholder="Type your name here..."
+        value={name}
+        required
+      />
+    );
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {playerNameElement}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{!isEditing ? "Edit" : "Save"}</button>
     </li>
   );
 }
