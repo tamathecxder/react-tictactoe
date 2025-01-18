7402ffd7397ef3539400e28bb51ee2076999de73
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-01-18 14:45:11 +0700

    split into single component for player list item

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import Player from "./components/Player";
+
 function App() {
   return (
     <>
@@ -9,20 +11,8 @@ function App() {
       <main>
         <div id="game-container">
           <ol id="players">
-            <li>
-              <span className="player">
-                <span className="player-name">Player 1</span>
-                <span className="player-symbol">X</span>
-              </span>
-              <button>Edit</button>
-            </li>
-            <li>
-              <span className="player">
-                <span className="player-name">Player 2</span>
-                <span className="player-symbol">Y</span>
-              </span>
-              <button>Edit</button>
-            </li>
+            <Player name="Player 1" symbol="X" />
+            <Player name="Player 2" symbol="Y" />
           </ol>
           Game Board
         </div>
