3ed2fb674de7ad77ce07c6fe70a4d6a10ac89d67
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-01-19 19:22:52 +0700

    set log

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,3 +1,11 @@
-export default function Log() {
-  retun(<ol id="log"></ol>);
+export default function Log({ turns }) {
+  return (
+    <ol id="log">
+      {turns.map((turn, index) => (
+        <li key={index}>
+          {turn.player} selected {turn.square.row}, {turn.square.col}
+        </li>
+      ))}
+    </ol>
+  );
 }
